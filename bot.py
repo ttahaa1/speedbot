3f819e98c87a300d3d6c2366ae77c98f855020be
ttahaa1 <153678863+ttahaa1@users.noreply.github.com>
2024-01-11 18:04:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,11 @@
+import os
 from telegram.ext import Updater, CommandHandler
 
-def start(update, context):
-    update.message.reply_text('Hello! I am your Telegram Bot.')
-
 def main():
-    # Replace 'YOUR_BOT_TOKEN' with your actual bot token
-    updater = Updater(token='YOUR_NEW_BOT_TOKEN', use_context=True)
-    dp = updater.dispatcher
+    # استخدام os.environ للوصول إلى متغير التكوين
+    updater = Updater(token=os.environ.get('TELEGRAM_BOT_TOKEN'), use_context=True)
 
-    dp.add_handler(CommandHandler("start", start))
-
-    updater.start_polling()
-    updater.idle()
+    # باقي الكود...
 
 if __name__ == '__main__':
     main()
