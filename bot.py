64e3767399a2bd8e483e86a56eb4fe80c1129ed2
ttahaa1 <153678863+ttahaa1@users.noreply.github.com>
2024-01-11 18:08:19 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,20 @@
 import os
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your bot.")
 
 def main():
     # استخدام os.environ للوصول إلى متغير التكوين
     updater = Updater(token=os.environ.get('TELEGRAM_TOKEN'), use_context=True)
+    
+    # اضف الأمر الذي تريد تخصيصه
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
 
-    # باقي الكود...
+    # بدء التحديث
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
     main()
